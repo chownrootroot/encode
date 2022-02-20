be8d9212e889b61ba5c3f2f50715f47de10362e6
chownrootroot <chownrootroot@tutanota.com>
2022-02-20 16:06:27 +0100

    - Moved file deletion to last so no files will be lost if script is aborted - Added status check on critical operations - Changed newlines in status prints to look nicer - Set delete flag to 1 as standard - Set a path to private key as standard

--- encode.sh
+++ encode.sh
@@ -2,6 +2,8 @@
 
 strDelete=0
 strMode=0
+strPrivkey=${HOME}/private.pem
+strDelete="1"
 
 # Read in arguments
 until [ -z "${1}" ]; do
@@ -11,8 +13,8 @@ until [ -z "${1}" ]; do
     elif [ "${1}" = "--path" ]; then
         shift
         strPath="${1}"
-    elif [ "${1}" = "--delete" ]; then
-        strDelete="1"
+    elif [ "${1}" = "--keep" ]; then
+        strDelete="0"
     elif [ "${1}" = "-e" ]; then
         strMode="1"
     elif [ "${1}" = "-d" ]; then
@@ -20,6 +22,13 @@ until [ -z "${1}" ]; do
      fi
     shift
 done
+
+function error(){
+	if [ "${?}" != "0" ]; then
+		printf "Operation was unsuccessful.\nExiting."
+		exit 1
+	fi
+}
 
 function encrypt(){
 	if [ ! -d "${strPath}" ]; then
@@ -34,14 +43,14 @@ function encrypt(){
 	fi
 
 	# Generate public key
-	printf "Generating public key"
-	openssl rsa -in ${strPrivkey} -outform PEM -pubout -out public.pem
+	printf "Generating public key\n"
+	openssl rsa -in ${strPrivkey} -outform PEM -pubout -out public.pem || error
 	printf "..done\n"
 	
 
 	# Generate password
 	printf "Generating password"
-	openssl rand -hex 32 > pass
+	openssl rand -hex 32 > pass || error
 	printf "..done\n"
 	
 	# Encrypt files
@@ -50,27 +59,30 @@ function encrypt(){
 	    strTrimmed=$(echo ${file} | awk -F "/" '{print $NF}')
 	    iFileNo=$(($iFileNo+1))
 	    printf "Encrypting ${file}.."
-	    openssl enc -aes-256-cbc -salt -in "${file}" -out "${strPath}${iFileNo}.aes" -pass file:pass
+	    openssl enc -aes-256-cbc -salt -in "${file}" -out "${strPath}${iFileNo}.aes" -pass file:pass || error
 	    printf "%s;%s\n" ${iFileNo} "${strTrimmed}" >> files 
 	    printf "..done\n"
-	    if [ "${strDelete}" = "1" ]; then
-	    	rm "${file}"
-	    fi
 	done
 
 	# Encrypt password
 	printf "Encrypting password"
-	openssl rsautl -encrypt -inkey public.pem -pubin -in pass -out ${strPath}_encrypted_password
+	openssl rsautl -encrypt -inkey public.pem -pubin -in pass -out ${strPath}_encrypted_password || error
 	printf "..done\n"
 
 	# Encrypt file list
 	printf "Encrypting file list"
-	openssl enc -aes-256-cbc -salt -in files -out ${strPath}_files -pass file:./pass
+	openssl enc -aes-256-cbc -salt -in files -out ${strPath}_files -pass file:./pass || error
 	printf "..done\n"
 
 	# Delete files
-	printf "Cleaning password and file list..."
-	rm pass files public.pem
+	printf "Cleaning password and file list...\n"
+	if [ "${strDelete}" = "1" ]; then
+		while read line; do
+			strFilename=$(echo ${line} | awk -F ";" '{print $2}')	
+			rm "${strPath}${strFilename}" && printf "Deleted %s\n" "${strFilename}" || error
+		done < files
+	fi
+	rm pass files public.pem || error
 	printf "..done\n"
 
 }
@@ -91,13 +103,13 @@ function decrypt(){
 	fi
 
 	# Decrypt password
-	printf "Decrypting password..."
-	openssl rsautl -decrypt -inkey ${strPrivkey} -in "${strPath}_encrypted_password" -out pass
+	printf "Decrypting password...\n"
+	openssl rsautl -decrypt -inkey ${strPrivkey} -in "${strPath}_encrypted_password" -out pass || error
 	printf "..done\n"
 	
 	# Decrypt file list
-	printf "Decrypting file list..."
-	openssl enc -d -aes-256-cbc -in "${strPath}_files" -out files -pass file:./pass
+	printf "Decrypting file list...\n"
+	openssl enc -d -aes-256-cbc -in "${strPath}_files" -out files -pass file:./pass || error
 	printf "..done\n"
 	
 	# Decrypt
@@ -105,17 +117,23 @@ function decrypt(){
 		strFileNo=$(echo "${line}" | awk -F ";" '{print $1}')			
 		strFileName=$(echo "${line}" | awk -F ';' '{print $2}')
                 printf "Decrypting ${strFileName}.."
-		openssl enc -d -aes-256-cbc -in "${strPath}${strFileNo}.aes" -out "${strPath}${strFileName}" -pass file:./pass
+		openssl enc -d -aes-256-cbc -in "${strPath}${strFileNo}.aes" -out "${strPath}${strFileName}" -pass file:./pass || error
 	        printf "..done\n"
-       	        if [ "${strDelete}" = "1" ]; then
-	    		rm "${strPath}${strFileNo}.aes"
-	        fi
+       	        #if [ "${strDelete}" = "1" ]; then
+	    	#	rm "${strPath}${strFileNo}.aes"
+	        #fi
 		
 	done < files
 
 	# Delete files
-	printf "Cleaning password and file list..."
-	rm ${strPath}_files ${strPath}_encrypted_password pass files	
+	printf "Cleaning password and file list...\n"
+	if [ "${strDelete}" = "1" ]; then
+		while read line; do
+			strFilename=$(echo ${line} | awk -F ";" '{print $1}')	
+			rm "${strPath}${strFilename}.aes" && printf "Deleted %s.aes\n" "${strFilename}" || error
+		done < files
+	fi
+	rm ${strPath}_files ${strPath}_encrypted_password pass files || error
 	printf "..done\n"
 
 }
